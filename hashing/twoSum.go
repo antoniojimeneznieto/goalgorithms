25e4db211e9a7612f3de3df2f7fd72b3d3c6e304
Antonio Jimenez <antonio.jimenez.nieto@gmail.com>
2024-10-20 19:14:58 +0200

    Implement twoSum using a double loop

--- hashing/twoSum.go
+++ hashing/twoSum.go
@@ -4,9 +4,10 @@ import "fmt"
 
 func main() {
 	nums := []int{2, 7, 11, 15}
-	target := 9
+	target := 22
 
 	fmt.Println(twoSum(nums, target))
+	fmt.Println(twoSumIterating(nums, target))
 }
 
 func twoSum(nums []int, target int) []int {
@@ -16,6 +17,17 @@ func twoSum(nums []int, target int) []int {
 			return []int{j, i}
 		}
 		m[v] = i
+	}
+	return nil
+}
+
+func twoSumIterating(nums []int, target int) []int {
+	for i, v := range nums {
+		for j, w := range nums[i+1:] {
+			if v+w == target {
+				return []int{i, i + j + 1}
+			}
+		}
 	}
 	return nil
 }
