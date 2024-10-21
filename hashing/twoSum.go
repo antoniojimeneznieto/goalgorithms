006b9d40afab82cdec0ccba312063d24ea9973a2
Antonio Jimenez <antonio.jimenez.nieto@gmail.com>
2024-10-21 16:53:13 +0200

    Use pointers for twoSum

--- hashing/twoSum.go
+++ hashing/twoSum.go
@@ -6,13 +6,13 @@ func main() {
 	nums := []int{2, 7, 11, 15}
 	target := 22
 
-	fmt.Println(twoSum(nums, target))
-	fmt.Println(twoSumIterating(nums, target))
+	fmt.Println(twoSum(&nums, target))
+	fmt.Println(twoSumIterating(&nums, target))
 }
 
-func twoSum(nums []int, target int) []int {
+func twoSum(nums *[]int, target int) []int {
 	m := make(map[int]int)
-	for i, v := range nums {
+	for i, v := range *nums {
 		if j, ok := m[target-v]; ok {
 			return []int{j, i}
 		}
@@ -21,9 +21,9 @@ func twoSum(nums []int, target int) []int {
 	return nil
 }
 
-func twoSumIterating(nums []int, target int) []int {
-	for i, v := range nums {
-		for j, w := range nums[i+1:] {
+func twoSumIterating(nums *[]int, target int) []int {
+	for i, v := range *nums {
+		for j, w := range (*nums)[i+1:] {
 			if v+w == target {
 				return []int{i, i + j + 1}
 			}
